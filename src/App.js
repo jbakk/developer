d40318583afb102586b96e0ce5c842b69d3f2aab
bakarytoure <bakarytoure57@gmail.com>
2022-07-23 17:30:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Hello Bakary John
+          Hello JB
         </a>
       </header>
     </div>
